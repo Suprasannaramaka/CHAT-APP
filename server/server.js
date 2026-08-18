50610aa0a77c0e0658129af12b703cc25093c7ef
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-08-18 15:52:16 +0530

    init

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import {userRouter  } from "./routes/routes.js";
-import { messageRouter } from "./routes/messageroutes.js";
+import { messageRouter } from "./routes/messageRoutes.js";
 import { Server } from "socket.io";
 //create Express App and HTTP server
 const app = express();
