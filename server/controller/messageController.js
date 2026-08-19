494daaaaffc33b65c990d426557c3c78ca793a73
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-08-19 23:47:58 +0530

    init

--- server/controller/messageController.js
+++ server/controller/messageController.js
@@ -81,7 +81,7 @@ export const sendMessage = async(req , res) =>
       let imageUrl;
       if(image) {
        const uploadResponse = await cloudinary.uploader.upload(image);
-       imageUrl =uploadResponse.secure_url;
+       imageUrl = uploadResponse.secure_url;
       }
       const newMessage =  await Message.create({
        senderId , receiverId , text , image: imageUrl 
