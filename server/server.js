b79c6478b97c4fd6fb22fee83a9e179d77914a40
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-08-20 00:11:22 +0530

    init

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import http from http;
+import http from "http";
 import { connectDB } from "./lib/db.js";
 import router from "./routes/routes.js";
 import messageRouter from "./routes/messageRoutes.js";
