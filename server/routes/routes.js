7291ef529f6cb87dae69e25d58817396dfd66413
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-08-19 16:47:35 +0530

    init

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,8 +4,8 @@ import { protectRoute } from "../middleware/auth.js";
 
  const router = express.Router();
 
-userRouter.post("/signup" , signup);
-userRouter.post("/login" , login);
-userRouter.put("/update-profile" , protectRoute , updateProfile);
-userRouter.get("/check" , protectRoute , checkAuth);
+router.post("/signup" , signup);
+router.post("/login" , login);
+router.put("/update-profile" , protectRoute , updateProfile);
+router.get("/check" , protectRoute , checkAuth);
 export default userRouter;
