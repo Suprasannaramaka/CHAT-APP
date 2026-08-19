4cc5009fdecd49e831f9f513252835914414b427
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-08-19 18:28:36 +0530

    init

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ io.on("connection" , (socket) =>
 //Middleware setup
 app.use(express.json({limit: "4mb"}));
 app.use(cors({
-     origin:  "https://chat-app-phi-sooty-8oh3joks6t.vercel.app" ,
+     origin:   "http://localhost:5173" ,
    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
