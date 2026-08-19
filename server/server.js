864d7d4504e496c3fc848dfa05375a3ca8c5b2f5
Ramaka.Suprasanna <ramakasuprasanna6@gmail.com>
2026-08-19 19:35:04 +0530

    init

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use(cors({
      origin:   "http://localhost:5173" ,
    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization" , "token"]
 }));
 //Routes setup
 app.get("/api/status", (req , res) => res.send("Server is live"));
